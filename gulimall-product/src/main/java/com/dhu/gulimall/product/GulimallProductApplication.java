083f3dab937ea601e2fbed160608ab5a6e34ee47
AsimpleMan0 <2233628467@qq.com>
2022-10-05 16:49:20 +0800

    feat[product数据库连接配置及测试]

---
+++ gulimall-product/src/main/java/com/dhu/gulimall/product/GulimallProductApplication.java
@@ -0,0 +1,20 @@
+package com.dhu.gulimall.product;
+
+/*
+ *   @Author: Cosmos
+ *   @Date: 2022/10/5 16:21
+ */
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("com.dhu.gulimall.product.dao")
+@SpringBootApplication
+public class GulimallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallProductApplication.class, args);
+    }
+
+}
